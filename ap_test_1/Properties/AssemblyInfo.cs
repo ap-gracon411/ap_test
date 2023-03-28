947a2c91726285642eeaa8dafc232466cef0970f
PeatA <PeatA@9H3Z0G2>
2023-03-28 14:30:00 +0200

    Add project files.

---
+++ ap_test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ap_test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("ap_test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("664E2A9E-3395-40C0-BBEA-E0ECEA32BE68")]
+[assembly: AssemblyVersion("1.0.0.0")]
